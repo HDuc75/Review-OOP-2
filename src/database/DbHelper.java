ce9da4aa54f75b44984927936ef91c53d5b1de89
HP <HP@DESKTOP-3CNUUME>
2025-09-13 22:10:06 +0700

    Update Code

--- src/database/DbHelper.java
+++ src/database/DbHelper.java
@@ -1,5 +1,32 @@
 package database;
 
-public class DbHelper {
+import java.sql.*;
 
+public class DbHelper {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Warning: MySQL driver not found on classpath. Add mysql-connector-java.jar");
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DbConfig.DB_URL, DbConfig.DB_USER, DbConfig.DB_PASS);
+    }
+
+    public static void createTableIfNotExists() throws SQLException {
+        String sql = "CREATE TABLE IF NOT EXISTS students (" +
+                "student_id VARCHAR(10) PRIMARY KEY," +
+                "full_name VARCHAR(100)," +
+                "dob DATE," +
+                "major VARCHAR(10)," +
+                "gpa DOUBLE," +
+                "class_name VARCHAR(50)" +
+                ")";
+        try (Connection conn = getConnection(); Statement st = conn.createStatement()) {
+            st.execute(sql);
+        }
+    }
 }
+
